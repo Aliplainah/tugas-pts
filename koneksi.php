7c7bd8c2460b59cc89c2826ee66df77eeba7b5f3
Aliplainah <157385042+Aliplainah@users.noreply.github.com>
2024-03-12 10:32:54 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbnama = 'formulir';
+
+    $con = mysqli_connect($hostname,$username,$password,$dbnama) or die ('gagal terhubung');
+?>
